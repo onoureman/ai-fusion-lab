d87878265ecb694c015a270a7069e89588c9c7b9
onoureman <emansd8@gmail.com>
2025-10-09 15:18:56 +0200

    saved user pref to firebase for ai models

--- app/provider.js
+++ app/provider.js
@@ -1,51 +1,51 @@
 "use client"
-import React, { use } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import {AppSidebar} from './_components/AppSidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from './_components/AppSidebar';
 import AppHeader from './_components/AppHeader';
-import App from 'next/app';
 import { useUser } from '@clerk/nextjs';
-import { useEffect } from 'react';
 import { db } from '@/config/FirebaseConfig';
-
+import { AiSelectedModelContext } from '@/context/AiSelectedModelContext';
+import { defaultModel as DefaultModel } from '@/shared/AiModelsShared';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { UserDetailContext } from '@/context/UserDetailContext';
 
 function Provider({ children, ...props }) {
+  const { user } = useUser();
+  const [aiSelectedModels, setAiSelectedModels] = useState(DefaultModel);
+  const [userDetail, setUserDetail] = useState();
 
-const {user} = useUser();
+  useEffect(() => {
+    if (user) {
+      CreateNewUser();
+    }
+  }, [user]);
 
-useEffect(() => {
-  if (user) {
-    CreateNewUser();
-  }
-  }, [user])
- 
-
-
-  const CreateNewUser = async() => {
-    //if user exist
-    const userRef=doc(db,'users',user?.primaryEmailAddress?.emailAddresses);
-    const userSnap=await getDoc(userRef);
-    if(userSnap.exists()){
+  const CreateNewUser = async () => {
+    const userKey = user?.primaryEmailAddress?.emailAddress || user?.primaryEmailAddress?.emailAddresses || user?.id;
+    const userRef = doc(db, 'users', userKey);
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) {
       console.log('user already exist');
+      const userInfo = userSnap.data();
+      setAiSelectedModels(userInfo?.selectedModelPref || DefaultModel);
+      setUserDetail(userInfo);
       return;
-
     } else {
-       const userData = {
+      const userData = {
         name: user?.fullName,
         email: user?.primaryEmailAddress?.emailAddress,
         createdAt: new Date(),
-        remainingMsg:5,
-        plan:'free',
-        credits:1000
+        remainingMsg: 5,
+        plan: 'free',
+        credits: 1000
+      };
+      await setDoc(userRef, userData);
+      console.log('new user created');
+      setUserDetail(userData);
     }
-    await setDoc(userRef,userData);
-    console.log('new user created');
-    
-  }
- 
-}
+  };
 
   return (
     <NextThemesProvider
@@ -53,18 +53,20 @@ useEffect(() => {
       defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
-
-    ><SidebarProvider>
-      <AppSidebar />
-
-      <div className='w-full'>
-        <AppHeader />
-        {children} 
-      </div>
-    </SidebarProvider>
+    >
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+        <AiSelectedModelContext.Provider value={{ aiSelectedModels, setAiSelectedModels }}>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className='w-full'>
+              <AppHeader />
+              {children}
+            </div>
+          </SidebarProvider>
+        </AiSelectedModelContext.Provider>
+      </UserDetailContext.Provider>
     </NextThemesProvider>
   );
 }
 
 export default Provider
-    
